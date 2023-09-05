756d3a6f1e62fe7069492736423189689de29fde
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-09-05 11:32:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function volume_sphere() {
     // Get the radius value from the input field
     const radius = parseFloat(document.getElementById('radius').value);
-	const vol =( 4/3)*Math.PI*radius*radius*radius
-    document.getElementById('volume').value = vol.toFixed(2) ; 
+	const vol =4/3*Math.PI*radius*radius*radius
+    document.getElementById('volume').value = vol.toFixed(4) ; 
 
     return false;
 }
